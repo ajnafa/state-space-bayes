12bf8027d3ce84e7dea38399c125de35def8fad8
Nafa <adamnafa@my.unt.edu>
2022-01-30 18:27:53 -0600

    Add code for the deterministic local linear univariate SSM

---
+++ scripts/01_Deterministic_Local_Linear_Univariate_SSM.R
@@ -0,0 +1,142 @@
+#------------State Space Models in R and Stan: Local Level Models---------------
+#-Author: A. Jordan Nafa-----------------------------Created: January 29, 2022-#
+#-R Version: 4.1.0-----------------------------------Revised: January 30, 2022-#
+
+# Set Project Options----
+options(
+  digits = 4, # Significant figures output
+  scipen = 999, # Disable scientific notation
+  repos = getOption("repos")["CRAN"],
+  mc.cores = 12L
+)
+
+# Load the necessary libraries----
+pacman::p_load(
+  "tidyverse",
+  "data.table",
+  "dtplyr",
+  "cmdstanr",
+  "tidybayes",
+  "latex2exp"
+)
+
+# Directory for the model files
+models_dir <- "models/Deterministic Local Linear SSM/"
+
+# Load the helper functions
+source("scripts/00_Helper_Functions.R")
+
+#------------------------------------------------------------------------------#
+#------------------------------Data Pre-Processing------------------------------
+#------------------------------------------------------------------------------#
+
+# Load the V-Dem v. 11.1 Data
+data(vdem, package = "vdemdata")
+
+# Data Preparation
+vdem_df <- vdem %>% 
+  # Keep only observations for the United States
+  filter(country_text_id == "USA") %>% 
+  # Extract the liberal democracy index
+  transmute(
+    # Country Abbreviations
+    country = country_text_id,
+    # Observation Year
+    time = year,
+    # Liberal Democracy Index
+    libdem = v2x_libdem * 100,
+    # Liberal Democracy Index Measurement Noise
+    libdem_sd = v2x_libdem_sd * 100,
+    # Calculate log and delta
+    across(
+      libdem,
+      list(
+        delta = ~ .x - lag(.x, n = 1L),
+        lagt1 = ~ lag(.x, n = 1L),
+        log = ~ log(.x)
+      ),
+      .names = "{.col}_{.fn}"
+    )
+  ) %>% 
+  # Drop NA values
+  drop_na()
+
+#------------------------------------------------------------------------------#
+#--------------Deterministic Local Level Linear State-Space Model---------------
+#------------------------------------------------------------------------------#
+
+# Load the Stan Model File
+DLL_SSM <- "stan/Deterministic_Local_Linear_Univariate_SSM.stan"
+
+# Compile the Stan model
+dll_ssm_mod <- cmdstan_model(
+  DLL_SSM, 
+  dir = models_dir,
+  force_recompile = TRUE
+  )
+
+# Print the model code
+str_split(dll_ssm_mod$code(), pattern = ";", simplify = T)
+
+# Prepare the Data to Pass to Stan
+stan_data <- list(
+  N = nrow(vdem_df),
+  y = vdem_df$libdem_delta,
+  sigma_y = sd(vdem_df$libdem_delta)*2.5
+)
+
+# Fit the Stan Model
+dll_ssm_fit <- dll_ssm_mod$sample(
+  data = stan_data,
+  seed = 123456,
+  refresh = 50,
+  output_dir = models_dir,
+  sig_figs = 5,
+  parallel_chains = 6,
+  chains = 6,
+  iter_warmup = 2000,
+  iter_sampling = 2000,
+  max_treedepth = 11
+)
+
+# Write the model object to an RDS file
+dll_ssm_fit$save_object(file = str_c(models_dir, "dll_ssm_fit.rds"))
+
+# Extract the Posterior Draws
+draws <- dll_ssm_fit$draws(variables = c("mu", "sigma"), format = "df")
+
+# Print a Summary of the Posterior Draws
+(summ_draws <- summarise_draws(draws))
+
+#------------------------------------------------------------------------------#
+#--------------------------Posterior Predictions--------------------------------
+#------------------------------------------------------------------------------#
+
+# Plot the change in democracy over the previous year
+ggplot(vdem_df, aes(x = time)) +
+  # Difference between observed and predicted
+  geom_line(
+    aes(y = libdem_delta - summ_draws$mean[1]), 
+    size = 1.1, 
+    color = "blue",
+    alpha = 0.75
+    ) +
+  # Observed Change
+  geom_line(
+    aes(y = libdem_delta), 
+    size = 1.1, 
+    color = "red", 
+    alpha = 0.75
+    ) +
+  # Add custom theme settings
+  plot_theme(plot.margin = margin(5, 1, 3, 3, "mm")) +
+  # Add labels to the plot
+  labs(
+    x = "Time",
+    y = "Liberal Democracy",
+    subtitle = "Figure 1.1 Democracy in America (1779 - 2020)"
+  ) +
+  # Adjust the breaks on the x axis
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
+  # Adjust the breaks on the y axis
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
---
+++ stan/Deterministic_Local_Linear_Univariate_SSM.stan
@@ -0,0 +1,29 @@
+// Deterministic Local Level Linear State-Space Model in Stan
+data {
+  int<lower = 1> N; // Number of Observations in the Time Series
+  vector[N] y; // The Response Vector
+  real sigma_y; // Standard Deviation of the Response Vector
+}
+
+parameters {
+  real mu; // Level Component
+  real<lower = 0> sigma; // Disturbance Component
+}
+
+model {
+  // Priors
+  target += normal_lpdf(mu | 0, 3);
+  target += normal_lpdf(sigma | 0, sigma_y);
+  
+  // Gaussian Likelihood
+  target += normal_lpdf(y | mu, sigma);
+}
+
+generated quantities {
+  vector[N] log_lik; // Pointwise Log Likelihood
+
+  // Calculate the Pointwise Log Likelihood
+  for (n in 1:N) {
+    log_lik[n] = normal_lpdf(y[n] | mu, sigma);
+  }
+}
